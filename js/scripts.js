902a3c9e70838b1bca47c8fe4b8750a16d18f680
Drew Henderson <drewhendersonemail@gmail.com>
2021-11-05 16:49:02 -0700

    added remove toppings button

--- js/scripts.js
+++ js/scripts.js
@@ -17,36 +17,41 @@ Pizza.prototype.calcCost = function() {
   return (this.toppings.length * size * 0.25 + size + 5);
 };
 
-let newPizza = new Pizza();
-console.log(newPizza);
+Pizza.prototype.listToppings = function() {
+  $("#showOrder").html("Purchase a " + newPizza.size + " pizza with ");
+  if (newPizza.toppings.length === 0) {
+    $("#showOrder").append("no toppings ");
+  }
+  else if (newPizza.toppings.length === 1) {
+    $("#showOrder").append(newPizza.toppings[0] + " ");
+  }
+  else {
+    for (let i = 0; i < newPizza.toppings.length; i++){
+      if (newPizza.toppings.length === i + 1) {
+        $("#showOrder").append("and " + newPizza.toppings[i] + " ");
+      } else {
+        $("#showOrder").append(newPizza.toppings[i] + ", ");
+      }
+    }
+  }
+  $("#showOrder").append("for " + newPizza.calcCost() + "$?");
+}
 
+let newPizza = new Pizza();
 
 $(document).ready(function() {
   $("#toppingForm").submit(function(event) {
     event.preventDefault();
+    $("#remove").on("click", function() {
+      newPizza.toppings = [];
+    })
     newPizza.toppings.push($("input#topping").val())
     console.log(newPizza);
   });
   $("#pizzaForm").submit(function(event) {
     event.preventDefault();
     newPizza.size = ($("#size").val());
-    $("#showOrder").html("Purchase a " + newPizza.size + " pizza with ");
-    if (newPizza.toppings.length === 0) {
-      $("#showOrder").append("no toppings ");
-    }
-    else if (newPizza.toppings.length === 1) {
-      $("#showOrder").append(newPizza.toppings[0] + " ");
-    }
-    else {
-      for (let i = 0; i < newPizza.toppings.length; i++){
-        if (newPizza.toppings.length === i + 1) {
-          $("#showOrder").append("and " + newPizza.toppings[i] + " ");
-        } else {
-          $("#showOrder").append(newPizza.toppings[i] + ", ");
-        }
-      }
-    }
-    $("#showOrder").append("for " + newPizza.calcCost() + "$?");
+    newPizza.listToppings();
     console.log(newPizza);
   });
 });
